23671e6a1589eafcf025043e6e2822aaed4e05f5
unifolio0 <121424793+unifolio0@users.noreply.github.com>
2023-10-02 21:22:25 +0900

    [Silver I] Title: 카드 구매하기, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+p = list(map(int, input().split()))
+re = [0]*(n+1)
+re[1] = p[0]
+for i in range(2, n+1):
+    re[i] = max([x+y for x, y in zip(re[:i], p[i-1::-1])])
+print(re[n])
