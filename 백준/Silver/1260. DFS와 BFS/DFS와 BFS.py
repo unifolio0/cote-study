b65adb76e0833afbd6bf9ca1c10c624aba84beca
unifolio0 <121424793+unifolio0@users.noreply.github.com>
2023-10-28 09:46:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 92 ms, Memory: 32236 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+N, M, V = map(int, input().split())
+graph = [[] for i in range(N+1)]
+visit = [0]*(N+1)
+for m in range(M):
+    i, j = map(int, input().split())
+    graph[i] += [j]
+    graph[j] += [i]
+for i in range(N+1):
+    graph[i].sort()
+
+def dfs(v):
+    visit[v] = 1
+    print(v, end=" ")
+    for i in graph[v]:
+        if visit[i] == 0:
+            dfs(i)
+
+def bfs(v):
+    Q = []
+    Q.append(v)
+    while Q:
+        vis = Q.pop(0)
+        for i in graph[vis]:
+            if visit[i] == 0 and i not in Q:
+                Q.append(i)
+        visit[vis] = 1
+        print(vis, end=" ")
+
+dfs(V)
+print()
+visit = [0]*(N+1)
+bfs(V)
