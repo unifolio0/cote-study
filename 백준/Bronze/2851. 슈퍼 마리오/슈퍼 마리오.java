d11896e5ff79944324b9319c899b8501ac950332
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-07-04 08:49:59 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 120 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,24 +1,31 @@
 import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[11];
-        for(int i=1;i<=10;i++)
-            arr[i] = arr[i-1] + Integer.parseInt(br.readLine());
-        int dif = Integer.MAX_VALUE;
-        int answer = 0;
-        for(int i=10;i>0;i--){
-            int temp = Math.abs(100 - arr[i]);
-            if(dif > temp){
-                dif = temp;
-                answer = arr[i];
+        int[] total = new int[10];
+        total[0] = Integer.parseInt(br.readLine());
+        for (int i = 1; i < 10; i++) {
+            int a = Integer.parseInt(br.readLine());
+            total[i] = total[i - 1] + a;
+        }
+
+        int idx = 9;
+        for (int i = 0; i < 9; i++) {
+            if (Math.abs(total[i] - 100) < Math.abs(total[i + 1] - 100)) {
+                idx = i;
+                break;
             }
         }
-        bw.write(answer + "");
+
+        bw.write(String.valueOf(total[idx]));
+
+        br.close();
+
         bw.flush();
         bw.close();
-        br.close();
     }
 }
