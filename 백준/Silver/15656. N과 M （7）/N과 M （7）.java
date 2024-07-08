bf13212e28c30c012d1b6e9fdfe0f3cef3d75353
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-07-08 09:21:41 +0900

    [Silver III] Title: N과 M (7), Time: 752 ms, Memory: 163752 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int M;
+    static int N;
+    static int[] a;
+    static int[] res;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        a = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        a = Arrays.stream(Main.a).sorted().toArray();
+        res = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            track(i, 1);
+        }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void track(int i, int d) throws IOException {
+        res[d - 1] = a[i];
+        if (d == M) {
+            for (int j = 0; j < M; j++) {
+                bw.write(res[j] + " ");
+            }
+            bw.write("\n");
+        } else {
+            for (int j = 0; j < N; j++) {
+                track(j, d + 1);
+            }
+        }
+    }
+}
