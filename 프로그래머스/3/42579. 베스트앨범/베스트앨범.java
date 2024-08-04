f1fb9604f9d14d576e501439d20bd20b37dfd284
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-08-04 09:49:05 +0900

    [level 3] Title: 베스트앨범, Time: 6.53 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,64 +1,44 @@
 import java.util.*;
-class Solution {
+import java.util.stream.Collectors;
+
+public class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        List<Integer> answerTemp = new ArrayList<>();
-        //장르, 총 재생된 횟수
-        Map<String, Integer> map = new HashMap<>();
-
+        Map<String, Integer> genreCount = new HashMap<>();
+        Set<String> strings1 = genreCount.keySet();
+        Map<String, List<int[]>> genrePlayCount = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
-            if (map.containsKey(genres[i])) {
-                map.put(genres[i], map.get(genres[i]) + plays[i]);
+            String genre = genres[i];
+            genreCount.put(genre, genreCount.getOrDefault(genre, 0) + plays[i]);
+            if (genrePlayCount.containsKey(genre)) {
+                List<int[]> ints = genrePlayCount.get(genre);
+                ints.add(new int[]{plays[i], i});
             } else {
-                map.put(genres[i], plays[i]);
+                List<int[]> arr = new ArrayList<>();
+                arr.add(new int[]{plays[i], i});
+                genrePlayCount.put(genre, arr);
             }
         }
-        // 속한 노래가 많이 재생된 장르 순서
-        List<Integer> list = new ArrayList<>();
-        map.values().stream().sorted(Comparator.reverseOrder()).forEach(list::add);
-
-        List<String> rank = new ArrayList<>(); //genre
-        for (int i = 0; i < list.size(); i++) {
-            for (String key : map.keySet()) {
-                if (map.get(key).equals(list.get(i))) {
-                    rank.add(key);
-                }
+        List<String> strings = genreCount.keySet().stream()
+                .sorted((s1, s2) -> genreCount.get(s2) - genreCount.get(s1))
+                .collect(Collectors.toList());
+        List<Integer> ret = new ArrayList<>();
+        for (String s : strings) {
+            List<int[]> ints = genrePlayCount.get(s);
+            List<int[]> list = ints.stream().sorted((i1, i2) -> i2[0] - i1[0])
+                    .limit(2)
+                    .collect(Collectors.toList());
+            if (list.size() == 1) {
+                ret.add(list.get(0)[1]);
+            }
+            if (list.size() == 2) {
+                ret.add(list.get(0)[1]);
+                ret.add(list.get(1)[1]);
             }
         }
-
-        List<String> genre = Arrays.asList(genres);
-        for (String r : rank) {
-            List<Integer> index = new ArrayList<>();
-            for (String g : genre) {
-                if (g.equals(r)) {
-                    index.add(genre.indexOf(g));
-                    genres[genre.indexOf(g)] = "";
-                }
-            }
-            List<Integer> sort = new ArrayList<>();
-            for (Integer i : index) {
-                sort.add(plays[i]);
-            }
-            sort.sort(Comparator.reverseOrder());
-            if (sort.size() >= 2) {
-                answerTemp.add(sort.get(0));
-                answerTemp.add(sort.get(1));
-            } else if (!sort.isEmpty()) {
-                answerTemp.addAll(sort);
-            }
+        int[] r = new int[ret.size()];
+        for (int i = 0; i < ret.size(); i++) {
+            r[i] = ret.get(i);
         }
-
-        List<Integer> index = new ArrayList<>();
-        for (Integer ii : answerTemp) {
-            for (int i = 0; i < plays.length; i++) {
-                if (ii.equals(plays[i])) {
-                    index.add(i);
-                    plays[i] = -1;
-                }
-            }
-        }
-
-        answer = index.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return r;
     }
 }
