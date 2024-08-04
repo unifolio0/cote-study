8bd083bc258bc40592f3255b79b618bb922dd370
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-08-04 15:15:04 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int[] slice = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(slice);
+            answer[i] = slice[command[2] - 1];
+        }
         return answer;
     }
 }
