bd7807d19b962a6873adbd050fe4805cc61ab579
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-08-20 20:41:36 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,34 +1,43 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br =   new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[][] d;
+    static int[][] steps;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int[][] board = new int[N][3];
+        N = Integer.parseInt(br.readLine());
+        steps = new int[N][3];
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            steps[i][0] = Integer.parseInt(st.nextToken());
+            steps[i][1] = Integer.parseInt(st.nextToken());
+            steps[i][2] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i < N; i++) {
-            board[i][0] += Math.min(board[i - 1][1], board[i - 1][2]);
-            board[i][1] += Math.min(board[i - 1][0], board[i - 1][2]);
-            board[i][2] += Math.min(board[i - 1][0], board[i - 1][1]);
-        }
-
-        bw.write(String.valueOf(Math.min(Math.min(board[N - 1][0], board[N - 1][1]), board[N - 1][2])));
+        d = new int[N][3];
+        bw.write(String.valueOf(Math.min(Math.min(f(0, 0), f(0, 1)), f(0, 2))));
 
         br.close();
 
         bw.flush();
         bw.close();
+    }
+
+    private static int f(int n, int c) {
+        if (n == N) return 0;
+        if (d[n][c] != 0) return d[n][c];
+        if (c == 0) {
+            d[n][c] = Math.min(f(n + 1, 1), f(n + 1, 2)) + steps[n][c];
+        }
+        if (c == 1) {
+            d[n][c] = Math.min(f(n + 1, 0), f(n + 1, 2)) + steps[n][c];
+        }
+        if (c == 2) {
+            d[n][c] = Math.min(f(n + 1, 1), f(n + 1, 0)) + steps[n][c];
+        }
+        return d[n][c];
     }
 }
