75ae929c14e501e02fc0c16a05c0125eabb9b6e6
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-09-03 22:26:10 +0900

    [Bronze I] Title: 2진수 8진수, Time: 504 ms, Memory: 33516 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,15 +1,25 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        StringBuilder s = new StringBuilder(sc.nextLine());
 
-        BigInteger b2 = new BigInteger(s, 2);
-        String sum = b2.toString(8);
+        int j = 0;
+        if (s.length() % 3 != 0) {
+            j = 3 - (s.length() % 3);
+        }
 
+        for (int i = 0; i < j; i++) {
+            s.insert(0, "0");
+        }
+
+        StringBuilder sum = new StringBuilder();
+        for (int i = 0; i < s.length(); i+=3) {
+            sum.append((Character.getNumericValue(s.charAt(i)) * 4) + (Character.getNumericValue(s.charAt(i + 1)) * 2)
+                    + Character.getNumericValue(s.charAt(i + 2)));
+        }
         System.out.println(sum);
     }
 
