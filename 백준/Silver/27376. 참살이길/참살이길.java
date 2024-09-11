0d7646d9c9f54e92fe58b1ec04958a68b12d2b47
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-09-11 10:44:30 +0900

    [Silver IV] Title: 참살이길, Time: 564 ms, Memory: 53548 KB -BaekjoonHub

---
+++ 백준/Silver/27376. 참살이길/참살이길.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        List<Sin> sins = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            sins.add(new Sin(x, t, s));
+        }
+        sins.sort((s1, s2) -> s1.x - s2.x);
+        long time = 0;
+        int pos = 0;
+        for (int i = 0; i < k; i++) {
+            time += sins.get(i).x - pos;
+            if (sins.get(i).s > time) {
+                time += sins.get(i).s - time;
+            } else {
+                long i2 = time - sins.get(i).s;
+                time += ((i2 / sins.get(i).t) % 2) * (sins.get(i).t - (i2 % sins.get(i).t));
+            }
+            pos = sins.get(i).x;
+//            System.out.println(pos + " " + time);
+        }
+        time += n - pos;
+        bw.write(String.valueOf(time));
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+
+    static class Sin {
+        int x;
+        int t;
+        int s;
+
+        public Sin(int x, int t, int s) {
+            this.x = x;
+            this.t = t;
+            this.s = s;
+        }
+    }
+}
