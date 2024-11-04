5766ffdb9b6501cb946d7ccda942c94c2b81fc85
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-11-04 17:19:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 220 ms, Memory: 56140 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,40 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int K;
+    static int[] W;
+    static int[] V;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int[N + 1][K + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
-        int[] W = new int[N + 1];
-        int[] V = new int[N + 1];
+        W = new int[N + 1];
+        V = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
             st = new StringTokenizer(br.readLine());
             W[i] = Integer.parseInt(st.nextToken());
             V[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[K + 1][N + 1];
-
-        for (int i = 1; i < K + 1; i++) {
-            for (int j = 1; j < N + 1; j++) {
-                if (W[j] > i) {
-                    dp[i][j] = dp[i][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], V[j] + dp[i - W[j]][j - 1]);
-                }
-            }
-        }
-
-        bw.write(String.valueOf(dp[K][N]));
-
-        br.close();
+        bw.write(String.valueOf(f(N, K)));
 
         bw.flush();
+
         bw.close();
+        br.close();
+    }
+
+    static int f(int n, int k) {
+        if (n < 1) {
+            return 0;
+        }
+        if (dp[n][k] != -1) return dp[n][k];
+        if (W[n] > k) {
+            dp[n][k] = f(n - 1, k);
+            return dp[n][k];
+        }
+        int re = Math.max(f(n - 1, k), f(n - 1, k - W[n]) + V[n]);
+        dp[n][k] = re;
+        return dp[n][k];
     }
 }
