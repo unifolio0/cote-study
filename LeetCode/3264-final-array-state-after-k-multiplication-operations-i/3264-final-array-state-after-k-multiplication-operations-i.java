fa1848c0a180d8d76076721cd355a3647ddb6330
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-12-16 15:04:00 +0900

    Time: 1 ms (100.00%), Space: 45.1 MB (20.61%) - LeetHub

---
+++ LeetCode/3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        while (k > 0) {
+            int min = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[min]) {
+                    min = i;
+                }
+            }
+            nums[min] *= multiplier;
+            k--;
+        }
+        return nums;
+    }
+}
