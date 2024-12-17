165d77c95bbd7e942b4e02631afb583929567804
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2024-12-17 18:45:51 +0900

    [Gold I] Title: 계단 수, Time: 124 ms, Memory: 18956 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -4,37 +4,43 @@ import java.io.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        int[][][] dp = new int[N][10][1 << 10];
-        for (int i = 1; i < 10; i++) {
-            dp[0][i][1 << i] = 1;
-        }
-
-        for (int i = 1; i < N; i++) {
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N][10][1 << 10];
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < 1 << 10; k++) {
-                    if (j - 1 >= 0) {
-                        dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
-                    }
-                    if (j + 1 < 10) {
-                        dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
-                    }
-                    dp[i][j][k | (1 << j)] %= 1_000_000_000;
-                }
+                Arrays.fill(dp[i][j], -1);
             }
         }
         int count = 0;
-        for (int i = 0; i < 10; i++) {
-            count += dp[N - 1][i][1023];
-            count %= 1_000_000_000;
+        for (int i = 1; i < 10; i++) {
+            count = (count + f(N - 1, i, 1 << i)) % 1_000_000_000;
         }
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(count % 1_000_000_000));
 
         bw.flush();
 
         bw.close();
         br.close();
+    }
+
+    private static int f(int dep, int i, int visit) {
+        if (dep == 0) {
+            if (visit == 1023) return 1;
+            return 0;
+        }
+        if (dp[dep][i][visit] != -1) return dp[dep][i][visit];
+        int count = 0;
+        if (i > 0) {
+            count = (count + f(dep - 1, i - 1, visit | 1 << (i - 1))) % 1_000_000_000;
+        }
+        if (i < 9) {
+            count = (count + f(dep - 1, i + 1, visit | 1 << (i + 1))) % 1_000_000_000;
+        }
+        dp[dep][i][visit] = count % 1_000_000_000;
+        return dp[dep][i][visit];
     }
 }
