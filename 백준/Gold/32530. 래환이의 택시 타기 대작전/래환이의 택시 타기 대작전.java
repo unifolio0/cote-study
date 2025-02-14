1d7940a940fe4c880a9dc31e135eea850ed28796
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-02-14 16:12:53 +0900

    [Gold V] Title: 래환이의 택시 타기 대작전, Time: 544 ms, Memory: 62664 KB -BaekjoonHub

--- 백준/Gold/32530. 래환이의 택시 타기 대작전/래환이의 택시 타기 대작전.java
+++ 백준/Gold/32530. 래환이의 택시 타기 대작전/래환이의 택시 타기 대작전.java
@@ -1,38 +1,42 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = Integer.parseInt(scanner.nextLine());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<Integer> times = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
-            String[] parts = scanner.nextLine().split(":");
-            int hour = Integer.parseInt(parts[0]);
-            int minute = Integer.parseInt(parts[1]);
-            int totalMinutes = hour * 60 + minute; // HH:MM을 "총 분"으로 변환
-            times.add(totalMinutes);
+            StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+            int hour = Integer.parseInt(st.nextToken());
+            int minute = Integer.parseInt(st.nextToken());
+            pq.offer(hour * 60 + minute);
         }
-        scanner.close();
-
-        // 학생들의 원하는 시간을 오름차순 정렬
-        Collections.sort(times);
-
-        int taxis = 0; // 필요한 택시 수
-        int i = 0;     // 배열 탐색 인덱스
-
-        while (i < N) {
-            taxis++; // 새로운 택시 할당
-            int firstStudentTime = times.get(i);            // 현재 그룹의 기준 시간
-            int maxAllowedTime = firstStudentTime + 20;         // 그룹에 속할 학생들의 원하는 시간은 firstStudentTime부터 +20분 이내여야 함
-            int count = 0;
-            // 최대 3명까지, 그리고 해당 학생의 원하는 시간이 maxAllowedTime 이내인 경우 그룹에 포함
-            while (i < N && times.get(i) <= maxAllowedTime && count < 3) {
-                i++;
-                count++;
+        int count = 0;
+        while (!pq.isEmpty()) {
+            count++;
+            int time = pq.poll();
+            int nextTime = time + 20;
+            int tmp = 1;
+            while (!pq.isEmpty()) {
+                if (pq.peek() > nextTime) {
+                    break;
+                }
+                pq.poll();
+                tmp++;
+                if (tmp == 3) {
+                    break;
+                }
             }
         }
 
-        System.out.println(taxis);
+        bw.write(String.valueOf(count));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
     }
 }
