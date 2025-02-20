d13be186c9accb808dbe71252868d47f9b52e7d8
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-02-20 13:50:42 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 47672 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (set.contains(num)) {
+                bw.write( "1\n");
+            } else {
+                bw.write( "0\n");
+            }
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
