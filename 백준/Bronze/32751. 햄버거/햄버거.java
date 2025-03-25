6557a0a7341011cf3f95b3a53580a1e4254d89c7
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-03-25 11:08:40 +0900

    [Bronze II] Title: 햄버거, Time: 116 ms, Memory: 15464 KB -BaekjoonHub

--- 백준/Bronze/32751. 햄버거/햄버거.java
+++ 백준/Bronze/32751. 햄버거/햄버거.java
@@ -4,46 +4,47 @@ import java.io.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
+
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        
+        int[] nums = new int[4];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken()); 
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
-        
-        String S = br.readLine();
-        
-        boolean isValid = true;
-        
-        if (S.charAt(0) != 'a' || S.charAt(N - 1) != 'a') {
-            isValid = false;
+        for (int i = 0; i < 4; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < N - 1; i++) {
-            if (S.charAt(i) == S.charAt(i + 1)) {
-                isValid = false;
-                break;
+        String S = br.readLine();
+        if (N == 1) {
+            if (S.equals("a") && nums[0] > 0) {
+                bw.write("Yes");
+            } else {
+                bw.write("No");
+            }
+        } else {
+            if (S.charAt(0) != 'a' || S.charAt(N - 1) != 'a' || nums[0] < 2) {
+                bw.write("No");
+            } else {
+                boolean eat = true;
+                nums[0] -= 2;
+                for (int i = 1; i < N - 1; i++) {
+                    if (nums[S.charAt(i) - 'a'] <= 0 || S.charAt(i) == S.charAt(i - 1)) {
+                        eat = false;
+                        break;
+                    }
+                    nums[S.charAt(i) - 'a']--;
+                }
+                if (S.charAt(N - 2) == 'a') {
+                    eat = false;
+                }
+                if (eat) {
+                    bw.write("Yes");
+                } else {
+                    bw.write("No");
+                }
             }
         }
 
-        int[] count = new int[4];
-        for (int i = 0; i < N; i++) {
-            char ch = S.charAt(i);
-            if (ch == 'a') count[0]++;
-            else if (ch == 'b') count[1]++;
-            else if (ch == 'c') count[2]++;
-            else if (ch == 'd') count[3]++;
-        }
-
-        if (count[0] > A || count[1] > B || count[2] > C || count[3] > D) {
-            isValid = false;
-        }
-
-        bw.write(isValid ? "Yes\n" : "No\n");
-
         bw.flush();
+
         bw.close();
         br.close();
     }
