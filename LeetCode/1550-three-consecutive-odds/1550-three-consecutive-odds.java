1a11c8e37509476c76431d3eaa10618d18fefedd
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-05-11 11:24:16 +0900

    Time: 0 ms (100%), Space: 42.7 MB (13.32%) - LeetHub

---
+++ LeetCode/1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                count = 0;
+            } else {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
