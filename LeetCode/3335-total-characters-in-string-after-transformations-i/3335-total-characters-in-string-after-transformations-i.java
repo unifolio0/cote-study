412d91db4f3e8f751548f51ecd8159ed9a42e9fc
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-05-13 17:02:41 +0900

    Time: 673 ms (6.33%), Space: 72 MB (11.39%) - LeetHub

--- LeetCode/3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
+++ LeetCode/3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
@@ -1,24 +1,22 @@
 class Solution {
     int[][] dp;
     int mod = 1000000007;
-    int T;
     public int lengthAfterTransformations(String s, int t) {
         dp = new int[26][t + 1];
         int count = 0;
-        T = t;
         for (int i = 0; i < s.length(); i++) {
-            count = (count + f(s.charAt(i) - 'a', 0) % mod) % mod;
+            count = (count + f(s.charAt(i) - 'a', t) % mod) % mod;
         }
         return count;
     }
 
     private int f(int c, int t) {
-        if (t >= T) return 1;
+        if (t < 1) return 1;
         if (dp[c][t] != 0) return dp[c][t] % mod;
         if (c == 25) {
-            dp[c][t] = (f(0, t + 1) + f(1, t + 1)) % mod;
+            dp[c][t] = (f(0, t - 1) + f(1, t - 1)) % mod;
         } else {
-            dp[c][t] = f(c + 1, t + 1) % mod;
+            dp[c][t] = f(c + 1, t - 1) % mod;
         }
         return dp[c][t] % mod;
     }
